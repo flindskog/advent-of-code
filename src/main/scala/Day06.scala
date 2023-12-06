ca2ab8298e3d5ebfb4df6f89b6ec4a7d0aaa7f28
Fredrik Lindskog <fredrik.lindskog@tundra.com>
2023-12-06 07:08:08 +0100

    Day 6

---
+++ src/main/scala/Day06.scala
@@ -0,0 +1,58 @@
+import util.Input
+
+import scala.annotation.tailrec
+
+trait Day06 {
+  val data           = Input.read("input_06.txt").toList
+  val times          = data(0).dropWhile(_ != ':').tail.trim.split(" +").map(_.toInt)
+  val records        = data(1).dropWhile(_ != ':').tail.trim.split(" +").map(_.toInt)
+  val timeAndRecords = times.zip(records)
+}
+
+object Day06_1 extends App with Day06 {
+  val result = timeAndRecords.map { (time, record) =>
+    (0 until time).map { speed =>
+      speed * (time - speed)
+    }.count(_ > record)
+  }.product
+
+  println(result)
+}
+
+object Day06_2 extends App with Day06 {
+  val time   = times.foldLeft("")((acc, time) => acc + time).toLong
+  val record = records.foldLeft("")((acc, time) => acc + time).toLong
+
+  @tailrec
+  def calcDistance(time: Long, speed: Long, accDistances: Seq[Long]): Seq[Long] = {
+    val distance = speed * (time - speed)
+    if (distance <= record) {
+      if (accDistances.nonEmpty && distance < accDistances.head) accDistances // Declining, we're done
+      else calcDistance(time, speed + 1, accDistances)
+    } else calcDistance(time, speed + 1, distance +: accDistances)
+  }
+
+  val minSpeed  = record / time
+  val distances = calcDistance(time, minSpeed, Seq.empty)
+
+  println(distances.size)
+}
+
+object Day06_2_2 extends App with Day06 {
+  val time   = times.foldLeft("")((acc, time) => acc + time).toLong
+  val record = records.foldLeft("")((acc, time) => acc + time).toLong
+
+  @tailrec
+  def find(time: Long, speed: Long, nxtFn: Long => Long): Long = {
+    val distance = speed * (time - speed)
+    if (distance > record) speed
+    else find(time, nxtFn(speed), nxtFn)
+  }
+  val minSpeed = record / time
+  val maxSpeed = time
+
+  val first = find(time, minSpeed, _ + 1)
+  val last  = find(time, maxSpeed, _ - 1)
+
+  println(last - first + 1)
+}
