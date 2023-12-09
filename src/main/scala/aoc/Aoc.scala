3d550b8cd85af529a46ba0ead870264d41455d9a
Fredrik Lindskog <fredrik.lindskog@tundra.com>
2023-12-09 15:45:02 +0100

    prep

--- src/main/scala/aoc/Aoc.scala
+++ src/main/scala/aoc/Aoc.scala
@@ -3,7 +3,18 @@ package aoc
 import aoc.utils.Input
 
 trait Aoc(fileName: String) extends App:
-  // it will show the execution time before exiting
+  // Shows the execution time
   util.Properties.setProp("scala.time", "on")
 
   val input = Input.read(fileName)
+
+  extension (input: LazyList[String])
+    def splitByEmptyLine: LazyList[List[String]] =
+      input
+        .foldLeft(LazyList(List.empty[String])) {
+          case (acc, "")   => List.empty[String] #:: acc
+          case (acc, line) => (line :: acc.head) #:: acc.tail
+        }
+        .map(_.reverse)
+        .reverse
+        .filter(_.nonEmpty)
---
+++ src/main/scala/aoc/y2023/Day10.scala
@@ -0,0 +1,4 @@
+package aoc.y2023
+
+object Day10 extends Aoc2023("input_10.txt"):
+  input.splitByEmptyLine.foreach(println)
