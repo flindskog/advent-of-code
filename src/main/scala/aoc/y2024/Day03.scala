e85c4941373e66e2b3d357e04c33ae8a5d3cb8e4
Fredrik Lindskog <fredrik.lindskog@tundra.com>
2024-12-03 06:32:05 +0100

    2024, day 3

--- src/main/scala/aoc/y2024/Day03.scala
+++ src/main/scala/aoc/y2024/Day03.scala
@@ -1,17 +1,41 @@
 package aoc.y2024
 
+import scala.annotation.tailrec
+import scala.util.Try
+
 object Day03 extends Aoc2024("input_03.txt"):
 
   val lineRegex = """mul\([0-9]{1,3},[0-9]{1,3}\)""".r
   val mulRegex  = """mul\(([0-9]{1,3}),([0-9]{1,3})\)""".r
 
+  val doMul: String => Long = s =>
+    Try {
+      val mulRegex(a, b) = s: @unchecked
+      a.toLong * b.toLong
+    }.getOrElse(0L)
+
   val result1 = input
     .flatMap(lineRegex.findAllIn)
-    .map: s =>
-      val mulRegex(a, b) = s
-      a.toLong * b.toLong
+    .map(doMul)
     .sum
 
   println(result1) // 164730528
 
-  
+  @tailrec
+  def clean(str: String, acc: String, collect: Boolean): String =
+    str match
+      case "" => acc
+      case s"do()$rest" =>
+        clean(rest, acc, true)
+      case s"don't()$rest" =>
+        clean(rest, acc, false)
+      case _ =>
+        if (collect) clean(str.tail, acc + str.head, true)
+        else clean(str.tail, acc, false)
+
+  val result2 = lineRegex
+    .findAllIn(clean(input.mkString, "", true))
+    .map(doMul)
+    .sum
+
+  println(result2) // 70478672
